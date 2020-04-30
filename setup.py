fd26f3e91eca37b4216f9b6800aca36e868f26ec
Nikita Romanov <nromanov@smartlabs.tv>
2020-04-30 23:47:19 +0300

    Add description content-type

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ from setuptools import setup
 
 setup(
     name='smart-assertions',
-    version='1.0.1',
+    version='1.0.2',
     description='Soft assertions for Python',
     long_description=io.open(os.path.join(os.path.dirname('__file__'), 'README.md'), encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
     author='balalaiQA',
     url='https://github.com/balalaiQA/smart-assertions',
     packages=['smart_assertions']
