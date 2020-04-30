39403ef0f1cf13f74071f3f80a4bc12cd8bd2772
Nikita Romanov <nromanov@smartlabs.tv>
2020-04-30 17:03:17 +0300

    Add travis setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='smart-assertions',
+    version='1.0.0',
+    description='Soft assertions for Python',
+    author='balalaiQA',
+    url='https://github.com/balalaiQA/smart-assertions',
+    packages=['smart_assertions']
+)
